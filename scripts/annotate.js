96fb84f8dbbeeabfcbd2dd922c60c3f62c986ff2
balolot.johnrey <balolot.johnrey@gmail.com>
2024-09-10 16:36:10 +0800

    added card shadow

--- scripts/annotate.js
+++ scripts/annotate.js
@@ -1,20 +1,20 @@
 import { annotate, annotationGroup } from 'https://unpkg.com/rough-notation?module';
 const tech = document.querySelector('#tech');
 const mern = document.querySelector('#mern');
-const cs = document.querySelector('#csharp');
+// const cs = document.querySelector('#csharp');
 const linkedIn = document.querySelector('#linkedIn');
 const facebook = document.querySelector('#facebook');
 const github = document.querySelector('#github');
 
 const a1 = annotate(tech, { type: 'underline', color: 'blue' });
 const a2 = annotate(mern, { type: 'circle', color: 'green' });
-const a3 = annotate(cs, { type: 'circle', color: 'purple' });
+// const a3 = annotate(cs, { type: 'circle', color: 'purple' });
 
 const li = annotate(linkedIn, {type: 'box', color: '#0a66c2'})
 const fb = annotate(facebook, {type: 'box', color: '#0866ff', padding: 6})
 const gh = annotate(github, {type: 'circle', color: '#151b23', padding: 8})
 
-const ag = annotationGroup([a2, a3, a1]);
+const ag = annotationGroup([a2, a1]);
 
 
 const main = document.querySelector('.main-container');
